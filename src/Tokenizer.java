7815296b085acd56a0635ef238620816d2d75c5a
lch159 <295169719@qq.com>
2017-09-29 20:42:08 +0800

    2017.9.29 20：41 pc

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -1,98 +1,72 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * Created by 李晨昊 on 2017/9/18.
- */
 public class Tokenizer {
-
     private static StringTokenizer stringTokenizer;
-    private static boolean change;
-    private static String line;
-    private static BufferedReader br;
-    private static File filename;
-    private static InputStreamReader reader;
-    private static String pathname;
 
+    public Tokenizer(String pathname) {
+        BufferedReader br = null;
 
-    public static void main(String[] args) {
         try {
-            pathname = "c:\\users\\29516\\Desktop\\test.txt"; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
-            filename = new File(pathname); // 要读取以上路径的input。txt文件
-            reader = new InputStreamReader(new FileInputStream(filename), "UTF-8"); // 建立一个输入流对象reader
-            br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言
-            line = "";
-            readNextLine();
+            File filename = new File(pathname);
+            InputStreamReader reader = new InputStreamReader(new FileInputStream(filename), "UTF-8");
+            br = new BufferedReader(reader);
+            String line = "";
+            StringBuffer content = new StringBuffer();
 
-            while ((line=br.readLine()) != null) {
-                change=false;
-
-                while (stringTokenizer.hasMoreTokens()) {
-                    System.out.println(nextToken().toLowerCase());
-                }
-
+            while ((line = br.readLine()) != null) {
+                content.append(line);
+                content.append("\r");
             }
+
+            String stringContent = new String(content);
+            stringContent = stringContent.replaceAll("[\\v\\t\\n\\r]", "-");
+            stringTokenizer = new StringTokenizer(stringContent, " ~,!@#$%^&*()_+{}:\\\".;/[]=-<>?！￥…（）—：”《》？·，。；’【】|、");
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Can not find the file");
+        } catch (IOException e) {
+            System.err.println("No such file or directory");
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Loading this file failed");
-        }
-
-    }
-
-    private static void readNextLine() {
-        try {
-            line = br.readLine();
-            stringTokenizer = new StringTokenizer(line, " ~,!@#$%^&*()_+{}:\\\".;/[]=-<>?！￥…（）—：”《》？·，。；’【】|、");
-            change = true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static boolean isChangedLine() {
-        return change;
-    }
-
-    private static String nextToken() {
-        String nextT = stringTokenizer.nextToken();
-
-        if (!stringTokenizer.hasMoreTokens()) {
-            readNextLine();
-        }
-//        System.out.println("1111"+stringTokenizer.hasMoreTokens());
-        if (nextT.contains("'")) {
-//            System.out.println("2222"+stringTokenizer.hasMoreTokens());
-            if (isChangedLine()){
-//                System.out.println("has changed +++++++");
-//                System.out.println("changed line-------"+stringTokenizer.nextToken());
-//                nextT=stringTokenizer.nextToken();
-//                return nextT;
+            System.err.println("Loading this file failed");
+        } finally {
+            try {
+                assert br != null;
+                br.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            String[] subStringTokenizer;
-            subStringTokenizer = nextT.split("'");
+        }
+    }
 
+    public String nextToken() {
+        String nextT = null;
+        if (stringTokenizer.hasMoreTokens()) {
+            nextT = stringTokenizer.nextToken();
 
+            if (nextT.contains("'")) {
 
-            int size = subStringTokenizer.length;
-            if (size==0)
-            {
-                return stringTokenizer.nextToken();
-            }
-            else if (size == 1) {
-                nextT = subStringTokenizer[0];
-            } else if (size >= 2) {
-                for (int i = 0; i < size - 1; i++) {
-                    if (!Objects.equals(subStringTokenizer[i], "") && !Objects.equals(subStringTokenizer[i + 1], "")) {
-                        return subStringTokenizer[i] + "'" + subStringTokenizer[i + 1];
-                    }
-                }
-                for (String elem : subStringTokenizer) {
-                    if (!Objects.equals(elem, ""))
-                        return elem;
+                String[] subStringTokenizer;
+                subStringTokenizer = nextT.split("'");
+
+                int size = subStringTokenizer.length;
+                if (size == 0)
+                    return nextToken();
+                else if (size == 1)
+                    nextT = subStringTokenizer[0];
+                else if (size >= 2) {
+                    for (int i = 0; i < size - 1; i++)
+                        if (!Objects.equals(subStringTokenizer[i], "") && !Objects.equals(subStringTokenizer[i + 1], ""))
+                            return subStringTokenizer[i] + "'" + subStringTokenizer[i + 1];
+
+                    for (String elem : subStringTokenizer)
+                        if (!Objects.equals(elem, ""))
+                            return elem;
                 }
             }
         }
-        return nextT;
+
+        return nextT != null ? nextT.toLowerCase() : null;
     }
 }
