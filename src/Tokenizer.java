b79a3eeb8ff553f65a31b1a4154af8f02bdc52ef
lch159 <295169719@qq.com>
2017-09-29 15:57:47 +0800

    2017.9.29 15:57 pc

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -8,42 +8,59 @@ import java.util.*;
  * Created by 李晨昊 on 2017/9/18.
  */
 public class Tokenizer {
+
+    private static StringTokenizer stringTokenizer;
+
     public static void main(String[] args) {
         try {
-
-
             String pathname = "c:\\users\\LLLLLLch\\Desktop\\dickens.txt"; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
             File filename = new File(pathname); // 要读取以上路径的input。txt文件
-            InputStreamReader reader = new InputStreamReader(new FileInputStream(filename)); // 建立一个输入流对象reader
+            InputStreamReader reader = new InputStreamReader(new FileInputStream(filename),"UTF-8"); // 建立一个输入流对象reader
             BufferedReader br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言
             String line = "";
 
-            String setSeparator = "-";
-            Map<String, List> mapList = new HashMap<>();
-
-            int i = 0;
             while ((line = br.readLine()) != null) {
-//                String[] words = line.split(";|:|!|\"|,|\'|.");
-                System.out.println(line);
-//                for (String word : words) {
-//                    System.out.print(word + "\n");
-//                }
-//                List<String> listWord = Arrays.asList(words);
-//                mapList.put("list" + i, listWord);
-//                i++;
-               line = br.readLine();
-                break;
+                stringTokenizer = new StringTokenizer(line, " ~,!@#$%^&*()_+{}:\\\".;/[]=-<>?！￥…（）—：”《》？·，。；’【】|、");
+                while (stringTokenizer.hasMoreTokens()) {
+                    System.out.println(nextToken().toLowerCase());
+                }
             }
-        }catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("文件读取失败");
-            }
-//            for (Map.Entry<String, List> mapping : mapList.entrySet()) {
-//            for (Object list : mapping.getValue()) {
-//                System.out.println(list);
-//            }
-//
-//        }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Loading this file failed");
+        }
 
+    }
+
+    private static String nextToken() {
+        String nextT = stringTokenizer.nextToken();
+
+        if (nextT.contains("'")) {
+            String[] subStringTokenizer;
+            if (nextT.equals("'")) {
+                if (stringTokenizer.hasMoreElements())
+                    return nextToken();
+            }
+            subStringTokenizer = nextT.split("'");
+            int size = subStringTokenizer.length;
+
+            if (size <= 0) {
+
+                return nextToken();
+            } else if (size == 1) {
+                nextT = subStringTokenizer[0];
+            } else if (size >= 2) {
+                for (int i = 0; i < size - 1; i++) {
+                    if (!Objects.equals(subStringTokenizer[i], "") && !Objects.equals(subStringTokenizer[i + 1], "")) {
+                        return subStringTokenizer[i] + "'" + subStringTokenizer[i + 1];
+                    }
+                }
+                for (String elem : subStringTokenizer) {
+                    if (!Objects.equals(elem, ""))
+                        return elem;
+                }
+            }
+        }
+        return nextT;
     }
 }
